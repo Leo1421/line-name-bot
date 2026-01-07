30f77413e1faf655c4e6a98d554af08b6f4160c9
Leo1421 <67752887+Leo1421@users.noreply.github.com>
2026-01-07 20:51:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def handle_message(event):
 
             flex_contents = {
                 "type": "bubble",
-                "size": "mega",
+                "size": "normal",
                 "body": {
                     "type": "box", "layout": "vertical", "paddingAll": "0px",
                     "contents": [
@@ -163,4 +163,5 @@ def callback():
 
 if __name__ == "__main__":
     app.run()
+
 
